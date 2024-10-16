afb8df5ffe71b2c2d95048bb5ad2161589ec1f54
Mix <32300164+mnixry@users.noreply.github.com>
2024-10-16 10:39:47 +0800

    Refactor entrypoint.sh to improve error handling and encapsulate user/group deletion logic

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-set -xve
+
+set -e
 
 USER=${USER:-"lean"}
 GROUP=${GROUP:-"lean"}
@@ -7,29 +8,29 @@ UID=${UID:-1000}
 GID=${GID:-1000}
 
 # there are serval cases we need to couple with
-# 1. duplicate user name, not same uid
+(
+    exec 2>/dev/null
 
-if [ $(getent passwd $USER) ]; then
-    userdel -r $USER
-fi
+    # 1. duplicate user name, not same uid
+    if [ $(getent passwd $USER) ]; then
+        userdel -r $USER
+    fi
 
-# 2. duplicate uid, not same name
+    # 2. duplicate uid, not same name
+    if [ $(getent passwd $UID) ]; then
+        userdel -r $(getent passwd $UID | cut -d: -f1)
+    fi
 
-if [ $(getent passwd $UID) ]; then
-    userdel -r $(getent passwd $UID | cut -d: -f1)
-fi
+    # 3. duplicate group name with different gid
+    if [ $(getent group $GROUP) ]; then
+        groupdel -f $GROUP
+    fi
 
-# 3. duplicate group name with different gid
-
-if [ $(getent group $GROUP) ]; then
-    groupdel -f $GROUP
-fi
-
-# 4. duplicate gid with different group name
-
-if [ $(getent group $GID) ]; then
-    groupdel -f $(getent group $GID | cut -d: -f1)
-fi
+    # 4. duplicate gid with different group name
+    if [ $(getent group $GID) ]; then
+        groupdel -f $(getent group $GID | cut -d: -f1)
+    fi
+)
 
 groupadd -g $GID $GROUP
 useradd -m -u $UID -g $GID $USER
